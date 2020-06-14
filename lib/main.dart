692c10be3bbce5ef5438d9cfc3a8a47b52b67766
Ahmed.hamdy <ahmed.saadeldin@aboughalymotors.com>
2020-06-14 19:21:44 +0200

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,36 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(),
-    );
-  }
+  runApp(MyHomePage());
 }
 
 class MyHomePage extends StatefulWidget {
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Chat"),
-      ),
-      body: Container(
-
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Column(
+        children: [
+          Container(
+            color: Color(0xFF553370),
+            child: Padding(
+                padding: EdgeInsets.only(top: 50.0, left: 20.0),
+                child: Text(
+                  "Connect Chat",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18.0,
+                      decoration: TextDecoration.none),
+                )),
+          )
+        ],
       ),
     );
   }
